0cafeef94bd3bdcf16360e9e364a3416378c6116
git-yash <yashmittalshah@gmail.com>
2023-04-11 17:14:16 -0500

    changed icon and text on button when clicked

--- src/components/ShowMapButton/ShowMapButton.tsx
+++ src/components/ShowMapButton/ShowMapButton.tsx
@@ -3,13 +3,31 @@ import './ShowMapButton.css';
 import {Fab} from "@mui/material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMap} from "@fortawesome/free-solid-svg-icons";
+import {faList} from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 
 const ShowMapButton = (props: { setMap: any }) => {
+    const [showMapText, setShowMapText] = useState("Show Map");
+    const [showMapIcon, setShowMapIcon] = useState(faMap);
+
+    function onShowMapClicked() {
+        props.setMap();
+
+        if(showMapText === "Show Map"){
+            setShowMapText("Show List");
+            setShowMapIcon(faList);
+        }
+        else {
+            setShowMapText("Show Map");
+            setShowMapIcon(faMap);
+        }
+    }
+
     return (
         <div className='map-button-container'>
-            <Fab variant='extended' className="map-button" onClick={props.setMap}>
-                <FontAwesomeIcon icon={faMap}/>
-                <div style={{paddingLeft: 5, fontFamily: "Poppins"}}>Show Map</div>
+            <Fab variant='extended' className="map-button" onClick={onShowMapClicked}>
+                <FontAwesomeIcon icon={showMapIcon}/>
+                <div style={{paddingLeft: 5, fontFamily: "Poppins"}}>{showMapText}</div>
             </Fab>
         </div>
     );
